58f9a85bc561420a64131fbc7b2bbf0d38ca694c
Matt Zanchelli <matt@mdznr.com>
2021-02-13 16:47:39 -0800

    Sorted directories on top, and files in a case insensitive manner.

--- fish/.config/fish/functions/sel.fish
+++ fish/.config/fish/functions/sel.fish
@@ -1,6 +1,7 @@
 # Interactively select a path (recursively)
 function sel
 	set -l selected_directory (pwd)
+	set selected_directory "$selected_directory/"
 
 	# If there is an argument, start in that path instead.
 	if [ "$argv[1]" != "" ]
@@ -8,20 +9,20 @@ function sel
 	end
 
 	while true 
-		set -l selected_subdirectory (ls -lahu $selected_directory | skip 1 | fzf --nth 9 | awk '{print $9}')
+		set -l selected_subdirectory (ls -lahuGp $selected_directory | skip 1 | awk '{printf("%6s %s %2s %s %-s\\n", $5, $6, $7, $8, $9);}' | fzf --nth 5 | awk '{print $5}')
 
 		# If the user didn’t select anything, they quit the command. Exit without doing anything.
 		if [ "$selected_subdirectory" = "" ]
 			break
 		end
 
-		# If the user selected “.”, that is the current directory. cd into that directory.
-		if [ "$selected_subdirectory" = "." ]
+		# If the user selected “.”, that is the current directory. Print that directory and exit.
+		if [ "$selected_subdirectory" = "./" ]
 			echo $selected_directory
 			break
 		end
 
-		set selected_directory "$selected_directory/$selected_subdirectory"
+		set selected_directory "$selected_directory$selected_subdirectory"
 
 		if test -d "$selected_directory"
 			# This is a directory. Keep navigating to build up the path.
